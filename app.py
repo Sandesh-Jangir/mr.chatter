3c5dd8aa850101b520dc6580baae4edfea8d1811
Sandesh-Jangir <sandeshatgithub@gmail.com>
2026-09-22 22:22:04 +0530

    added session chat history into the chatbot.

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ from groq import Groq
 
 client = Groq(api_key=os.environ.get("GROQ_API_KEY"))
 
+chat_history = []
 
 while True:
     user_input = input("You > ")
@@ -10,9 +11,12 @@ while True:
         print ("Mr. Chatter > Goodbye!")
         break
     
+    chat_history.append({"role": "user", "content": user_input})
     response = client.chat.completions.create(
         model = "openai/gpt-oss-20b",
-        messages = [{"role": "user", "content": user_input}]
+        messages = chat_history
     )
-    bot_reply = response.choices[0].message.content  
+    bot_reply = response.choices[0].message.content 
+    chat_history.append({"role": "assistant", "content": bot_reply}) 
+    
     print (f"Mr. Chatter > {bot_reply}")
